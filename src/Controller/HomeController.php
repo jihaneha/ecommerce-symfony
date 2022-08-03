34320fe1eedfb6c9e266807e78c263129262aa77
jihaneha <ghamidou800@gmail.com>
2022-08-03 11:39:03 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function homepage(ProductRepository $ProductRepository): Response
+    {
+        $products = $ProductRepository->findby([], [], 3);
+
+        return $this->render('home/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
